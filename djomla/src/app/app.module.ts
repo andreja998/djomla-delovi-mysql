d2a7025d9d0946284539bf1bfad4b18a13307c49
andreja998 <andrejaksp98@gmail.com>
2019-10-25 14:50:33 +0200

    New Angular project: bootstrap, popper.js, ng-select, jquery added

---
+++ djomla/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { PocetnaComponent } from './components/pocetna/pocetna.component';
+import { PretrazivacComponent } from './components/pretrazivac/pretrazivac.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    PaginationComponent,
+    PocetnaComponent,
+    PretrazivacComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgSelectModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ djomla/src/app/components/pretrazivac/pretrazivac.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pretrazivac',
+  templateUrl: './pretrazivac.component.html',
+  styleUrls: ['./pretrazivac.component.scss']
+})
+export class PretrazivacComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
